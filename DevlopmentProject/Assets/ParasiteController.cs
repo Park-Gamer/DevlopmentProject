c5a578d416e8069691df61a2690c1f80e332c5bf
Park <cjparkerdj@gmail.com>
2025-06-27 21:43:41 -0400

    Added movement and camera controls

--- DevlopmentProject/Assets/ParasiteController.cs
+++ DevlopmentProject/Assets/ParasiteController.cs
@@ -1,29 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class ParasiteController : MonoBehaviour
 {
     // Settings
-    public float MoveSpeed = 5;
-    public float SteerSpeed = 180;
-    public float BodySpeed = 5;
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    bool readyToJump;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    [SerializeField] private float playerHeight;
+    [SerializeField] private LayerMask whatIsGround;
+    bool grounded;
+
+    [SerializeField] private Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Start()
     {
-        float vertical = Input.GetAxis("Vertical");
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
 
-        Vector3 inputDirection = new Vector3(0f, 0f, vertical);
+        readyToJump = true;
+    }
 
-        if (inputDirection.magnitude >= 0.1f)
+    private void Update()
+    {
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+        MyInput();
+
+        // handle drag
+        if (grounded)
         {
-            // Move forward
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-
-            // Steer
-            float steerDirection = Input.GetAxis("Horizontal"); // Returns value -1, 0, or 1
-            transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+            rb.drag = groundDrag;
         }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
